1f9305acc5b6c80d475ebcca5e9ee9bb12b0be00
Agustin Garrone <agustinngarrone@hotmail.com>
2024-08-11 23:56:51 -0300

    responsive services

--- src/screen/servicios/components/specialties/index.tsx
+++ src/screen/servicios/components/specialties/index.tsx
@@ -1,31 +1,47 @@
-import { Flex, Img, Text } from "@chakra-ui/react"
-import { SPECIALITIES_DATA } from "./data"
-
+import { Flex, Img, Text } from "@chakra-ui/react";
+import { SPECIALITIES_DATA } from "./data";
 
 export const Specialties = () => {
-
-    return (
-        <Flex h="20em" w="80%" direction="column" alignItems="center" justifyContent="center" gap="3em">
-            <Text alignSelf="flex-start" color="white" fontSize="2rem">ESPECIALIDADES TÉCNICAS</Text>
-            <Flex direction={{ base: "column", md: "row" }} wrap="wrap" gap="4" w="100%">
-                {SPECIALITIES_DATA.map((s, index) => (
-                    <Flex
-                        gap="1em"
-                        key={index}
-                        alignItems="center"
-                        justifyContent="center"
-                        p={1}
-                        border="1px solid gray"
-                        borderRadius="15px"
-                        bg={s.color}
-                        color="white"
-                    >
-                        {/* Asegúrate de ajustar esto para que muestre los datos correctos */}
-                        <Img h="50%" src={s.img}/>
-                        <Text fontSize="md" fontWeight="bold">{s.title}</Text>
-                    </Flex>
-                ))}
-            </Flex>
-        </Flex>
-    )
-}
+  return (
+    <Flex
+      minH="20em"
+      w="80%"
+      direction="column"
+      alignItems="center"
+      mt={{ base: "20em", md: "6em", "2xl": "0" }}
+      justifyContent="center"
+      gap="3em"
+    >
+      <Text alignSelf="flex-start" color="white" fontSize="2rem">
+        ESPECIALIDADES TÉCNICAS
+      </Text>
+      <Flex
+        direction={{ base: "column", md: "row" }}
+        wrap="wrap"
+        gap="4"
+        w="100%"
+      >
+        {SPECIALITIES_DATA.map((s, index) => (
+          <Flex
+            gap="1em"
+            key={index}
+            alignItems="center"
+            justifyContent="center"
+            p={1}
+            border="1px solid gray"
+            borderRadius="15px"
+            h={{ md: "3.4em" }}
+            w={{ base: "100%", md: "20em" }}
+            bg={s.color}
+            color="white"
+          >
+            <Img h={{ base: "40%", md: "60%" }} src={s.img} />
+            <Text fontSize="md" fontWeight="bold">
+              {s.title}
+            </Text>
+          </Flex>
+        ))}
+      </Flex>
+    </Flex>
+  );
+};
